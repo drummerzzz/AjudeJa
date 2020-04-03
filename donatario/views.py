22105e28d7c55b269ab601158f88f001f0c3d737
Drummerzzz <jf.interatividade@gmail.com>
2020-04-03 00:28:04 -0300

    criando mensagem de confirmaçao de pedido

--- donatario/views.py
+++ donatario/views.py
@@ -6,8 +6,8 @@ from donatario.forms import GranteeForm
 class DonatarioCreateView(CreateView):
     form_class = GranteeForm
     template_name = "home/home.html"
-    success_url = "/"
+    success_url = "/#ajuda"
     
     def get_success_url(self):
-        messages.success(self.request, "Seu pedido foi enviado com sucesso, e já está disponível na fila de pedidos. <3")
+        messages.success(self.request, "Seu pedido foi enviado com sucesso e já está disponível na fila de pedidos.")
         return self.success_url
