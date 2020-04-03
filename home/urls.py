d4c4df7e9eff571ed0589836ba65383edacd04d5
Drummerzzz <jf.interatividade@gmail.com>
2020-04-02 23:18:33 -0300

    adicionando tela de detalhe

--- home/urls.py
+++ home/urls.py
@@ -1,21 +1,9 @@
-"""settings URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
-from home.views import HomeTemplateView
+from home.views import HomeTemplateView, DetailTemplateView
+
+app_name = 'home'
 
 urlpatterns = [
-  path('', HomeTemplateView.as_view())
+  path('', HomeTemplateView.as_view()),
+  path('detalhes/', DetailTemplateView.as_view(), name="detalhe")
 ]
--- home/views.py
+++ home/views.py
@@ -12,3 +12,15 @@ class HomeTemplateView(TemplateView):
         context['pedidos'] = Grantee.objects.all()
         return context
     
+    
+
+class DetailTemplateView(TemplateView):
+    template_name = "home/detalhe.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pedido_id = self.request.GET.get('pedido', None)
+        pedido = Grantee.objects.filter(id=pedido_id)[0]
+        context["pedido"] = pedido
+        return context
+    
