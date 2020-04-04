206a3b2498505229a3d61f231762d9a02cfa1424
Drummerzzz <jf.interatividade@gmail.com>
2020-04-04 15:57:46 -0300

    criando pagina de detail em semantic

--- home/views.py
+++ home/views.py
@@ -36,3 +36,10 @@ class DetailTemplateView(View):
 
 class Base(TemplateView):
     template_name = "baseUi.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = GranteeForm()
+        context['pedidos'] = Grantee.objects.all()
+        context['pedido'] = Grantee.objects.first()
+        return context
